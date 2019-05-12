61dda56599f37284e0c097435056165afc778071
Alexandra S <48199148+shrvas@users.noreply.github.com>
2019-05-12 17:16:56 +0300

    Add files via upload

---
+++ storage Sakharova.cpp
@@ -0,0 +1,37 @@
+#include "storage.h"
+
+template <typename T>
+Storage<T>::Storage(const T &val) {
+    SetVal(val);
+}
+template <typename T>
+void Storage<T>::SetVal(const T &val) {
+    m_val = val;
+}
+template <typename T>
+T Storage<T>::GetVal() {
+    return m_val;
+}
+template <typename T>
+Storage<T>::~Storage(){}
+
+Storage<char*>::Storage() {
+    m_val = nullptr;
+}
+Storage<char*>::Storage(const Storage& val) {
+    SetVal(val.m_val);
+}
+Storage<char*>::Storage(const char* val) {
+    SetVal(val);
+}
+void Storage<char*>::SetVal(const char* val) {
+    int c = 0;
+    while(val[c++]);
+    delete[] m_val;
+    m_val = new char[c];
+    memcpy(m_val,val,c);
+}
+char* Storage<char*>::GetVal() {
+    return m_val;
+}
+Storage<char*>::~Storage(){}
---
+++ storage Sakharova.h
@@ -0,0 +1,32 @@
+#include <cstring>
+template <typename T>
+class Storage
+{
+public:
+    Storage() = default;
+    Storage(const Storage& val) = default;
+    Storage(const T &val);
+    ~Storage();
+
+    void SetVal(const T &val);
+    T GetVal();
+private:
+    T m_val;
+};
+
+template <>
+class Storage<char*>
+{
+public:
+    Storage();
+    Storage(const Storage& val);
+    Storage(const char* val);
+    ~Storage();
+
+    void SetVal(const char* val);
+    char* GetVal();
+
+private:
+    char* m_val;
+};
+
